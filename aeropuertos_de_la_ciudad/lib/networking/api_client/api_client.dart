797673d688704ba9db77beb09f0b6cce13a06531
David Diaz <ddiazlink08@gmail.com>
2020-11-20 16:23:56 -0500

    I fix the api_client

--- aeropuertos_de_la_ciudad/lib/networking/api_client/api_client.dart
+++ aeropuertos_de_la_ciudad/lib/networking/api_client/api_client.dart
@@ -3,24 +3,16 @@ import 'package:aeropuertos_de_la_ciudad/Airport/model/airport.dart';
 
 class ApiClient {
   String _url = "https://airports-of-the-world.herokuapp.com";
-  String path = '/airports';
 
   Future<List<Airport>> getAirports() async {
-    Response response;
-    Dio dio = new Dio();
-    response = await dio.get('$_url$path');
-    final airports = (response.data['data']['data']);
+    Uri uri = Uri.https(
+      _url,
+      'airports',
+    );
+    final response = await Dio().getUri(uri);
 
-    return airports;
+    final airports =
+        new AllAirports.fromJsonList(response.data['data']['data']);
+    return airports.items;
   }
 }
-
-/*    Example:
-    Response response;
-    Dio dio = new Dio();
-    response = await dio.get("/test?id=12&name=wendu");
-    print(response.data.toString());
-    // Optionally the request above could also be done as
-    response = await dio.get("/test", queryParameters: {"id": 12, "name": "wendu"});
-    print(response.data.toString());
-*/
