a036d39de4ed9402ac3b16478cda8ce36425080c
David Diaz <ddiazlink08@gmail.com>
2020-11-26 21:01:26 -0500

    Fixing view

--- aeropuertos_de_la_ciudad/lib/homePage.dart
+++ aeropuertos_de_la_ciudad/lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:aeropuertos_de_la_ciudad/Airport/model/airport.dart';
 import 'Airport/bloc/airport_list_bloc.dart';
 
@@ -8,25 +9,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final AirportListBloc _airportBloc = AirportListBloc();
-
-  /* @override
-  void dispose() {
-    super.dispose();
-    //  _airportBloc.dispose();
-  }*/
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Aeropuertos de la Ciudad"),
       ),
-      body: Container(
-        child: StreamBuilder<List<Airport>>(
-            //   stream:
-            builder:
-                (BuildContext context, AsyncSnapshot<List<Airport>> snapshot) {
+      body: Container(child: StreamBuilder<List<Airport>>(
+        builder: (BuildContext context, AsyncSnapshot<List<Airport>> snapshot) {
           return ListView.builder(
             itemCount: 10,
             itemBuilder: (context, index) {
@@ -41,30 +31,14 @@ class _HomePageState extends State<HomePage> {
                         "${snapshot.data[index].airportId}.",
                         style: TextStyle(fontSize: 20.0),
                       ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            "${snapshot.data[index].airportName}",
-                            style: TextStyle(fontSize: 18.0),
-                          ),
-                          Text(
-                            "${snapshot.data[index].countryId}",
-                            style: TextStyle(fontSize: 16.0),
-                          )
-                        ],
-                      ),
-                    ),
+                    )
                   ],
                 ),
               );
             },
           );
-        }),
-      ),
+        },
+      )),
     );
   }
 }
