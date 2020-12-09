5cc7c28103a62aad12c644161047437847c342ea
David Diaz <ddiazlink08@gmail.com>
2020-12-09 13:23:24 -0500

    add limit of Airports

--- aeropuertos_de_la_ciudad/lib/networking/api_client/api_client.dart
+++ aeropuertos_de_la_ciudad/lib/networking/api_client/api_client.dart
@@ -4,12 +4,12 @@ import 'package:aeropuertos_de_la_ciudad/Airport/model/airport.dart';
 
 class ApiClient {
   String _url = "airports-of-the-world.herokuapp.com";
+  int _limit = 100;
 
   Future<List<Airport>> getAirports() async {
-    Uri uri = Uri.https(
-      _url,
-      'airports',
-    );
+    Uri uri = Uri.https(_url, 'airports', {
+      'limit': _limit.toString(),
+    });
     final response = await Dio().getUri(uri);
 
     final airports = new Airports.fromJsonList(response.data['data']['data']);
